ebe756331c32b1298448f2983607987376d00445
spiderLambo <spiderLambo.dev@outlook.com>
2024-10-01 21:52:02 +0200

    camera

--- object_3d.py
+++ object_3d.py
@@ -28,5 +28,38 @@ class Object3d:
             ]
         )
 
+    def screen_projection(self):
+        vertexes = self.vertexes @ self.render.camera.camera_matrix()
+        vertexes = vertexes @ self.render.projection_matrix
+        vertexes /= vertexes[:, -1].reshape(-1, 1)
+        vertexes[(vertexes > 1) | (vertexes < -1)] = 0
+        vertexes = vertexes @ self.render.projection.to_screen_matrix
+        vertexes = vertexes[:, :2]
+
+        for face in self.face:
+            polygon = vertexes[face]
+            if not np.any(
+                (polygon == self.render.H_WIDTH) | (polygon == self.render.H_HEIGHT)
+            ):
+                pg.draw.polygon(self.render.screen, pg.Color("orange"), polygon, 3)
+
+        for vertexe in vertexes:
+            if not np.any(
+                (vertexe == self.render.H_WIDTH) | (vertexe == self.render.H_HEIGHT)
+            ):
+                pg.draw.circle(self.render.screen, pg.Color("white"), vertexe, 6)
+
     def translate(self, pos):
         self.vertexes = self.vertexes @ translate(pos)
+
+    def scale(self, scale_to):
+        self.vertexes = self.vertexes @ scale(scale_to)
+
+    def rotate_x(self, angle):
+        self.vertexes = self.vertexes @ rotate_x(angle)
+
+    def rotate_y(self, angle):
+        self.vertexes = self.vertexes @ rotate_y(angle)
+
+    def rotate_z(self, angle):
+        self.vertexes = self.vertexes @ rotate_z(angle)
